dd59d27f6fcc35ac0b5356576c67edb7c49a09c0
Daniel Barros Sales <sales.b.daniel@gmail.com>
2020-03-18 21:32:14 -0300

    frist commit

---
+++ src/main/java/com/computeiros/educagreste/EducagresteApplication.java
@@ -0,0 +1,13 @@
+package com.computeiros.educagreste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducagresteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducagresteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/computeiros/educagreste/EducagresteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.computeiros.educagreste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducagresteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
